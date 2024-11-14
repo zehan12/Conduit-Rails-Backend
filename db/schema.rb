9f07f211b7d28094dd0c1f3914fc0e192be4f489
zehan12 <zehan9211@gmail.com>
2024-11-15 04:04:12 +0530

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_14_213519) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_14_223015) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_14_213519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "jti"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
