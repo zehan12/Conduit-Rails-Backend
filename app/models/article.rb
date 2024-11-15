faa24f6b9a73b0146a040451c4f2dff5878ba9bd
zehan12 <zehan9211@gmail.com>
2024-11-15 14:51:22 +0530

    update schema name from posts to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Post < ApplicationRecord
+class Article < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  has_many :posts, foreign_key: "author_id"
+  has_many :articles, foreign_key: "author_id"
   has_many :comments, foreign_key: :author_id, dependent: :destroy
 end
