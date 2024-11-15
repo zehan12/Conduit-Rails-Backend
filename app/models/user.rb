4ecaa19d7a37d2f547febef04cb85766c2c5817e
zehan12 <zehan9211@gmail.com>
2024-11-15 14:25:19 +0530

    create post schema and add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_many :posts, foreign_key: "author_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_15_004445) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_15_075758) do
+  create_table "posts", force: :cascade do |t|
+    t.string "slug"
+    t.string "title"
+    t.text "description"
+    t.text "body"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,4 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_15_004445) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "posts", "users", column: "author_id"
 end
